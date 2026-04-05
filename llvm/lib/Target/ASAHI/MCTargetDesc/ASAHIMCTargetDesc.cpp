add73a9c4fcc027cf5152fd512c6f4abfc1d88e6
Prashanth Srikanth Pujar <prashanth.pujar.usc@gmail.com>
2026-04-04 20:51:41 -0700

    [ASAHI] Plugged in the subtarget info
    Right now the subtarget info is just an empty shell, but it is
    required to enable the use of the legacy pass manager.
    The legacy pass manager is connected to the codegen path and codegen
    needs this kind of low level details.

--- llvm/lib/Target/ASAHI/MCTargetDesc/ASAHIMCTargetDesc.cpp
+++ llvm/lib/Target/ASAHI/MCTargetDesc/ASAHIMCTargetDesc.cpp
@@ -1,3 +1,24 @@
-#include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
 
-extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeASAHITargetMC() {}
+#include "ASAHIMCTargetDesc.h"
+#include "TargetInfo/ASAHITargetInfo.h" // getTheASAHITarget
+#include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/TargetParser/Triple.h"
+
+using namespace llvm;
+
+#define GET_SUBTARGETINFO_MC_DESC
+#include "ASAHIGenSubtargetInfo.inc"
+
+static MCSubtargetInfo *
+createASAHIMCSubtargetInfo(const Triple &TT, StringRef CPU, StringRef FS) {
+    return createASAHIMCSubtargetInfoImpl(TT, CPU, /*TuneCPU*/ CPU, FS);
+}
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeASAHITargetMC() {
+    Target &TheTarget = getTheASAHITarget();
+
+    // Register the MC subtarget info.
+    TargetRegistry::RegisterMCSubtargetInfo(TheTarget, createASAHIMCSubtargetInfo);
+}
---
+++ llvm/lib/Target/ASAHI/MCTargetDesc/ASAHIMCTargetDesc.h
@@ -0,0 +1,20 @@
+//===-- ASAHIMCTargetDesc.h - ASAHI Target Descriptions ---------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+/// Provides ASAHI specific target descriptions.
+//
+//===----------------------------------------------------------------------===//
+//
+
+#ifndef LLVM_LIB_TARGET_ASAHI_MCTARGETDESC_ASAHIMCTARGETDESC_H
+#define LLVM_LIB_TARGET_ASAHI_MCTARGETDESC_ASAHIMCTARGETDESC_H
+
+#define GET_SUBTARGETINFO_ENUM
+#include "ASAHIGenSubtargetInfo.inc"
+
+#endif
