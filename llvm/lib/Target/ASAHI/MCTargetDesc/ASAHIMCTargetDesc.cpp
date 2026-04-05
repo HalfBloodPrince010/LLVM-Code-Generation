e119dd843ee4c3a94c8b4134be913316f8a47d32
Prashanth Srikanth Pujar <prashanth.pujar.usc@gmail.com>
2026-04-04 20:56:38 -0700

    [ASAHI] Plugged the MCInstrInfo
    The MCInstrInfo is the based class used to produce the target specific
    MCInstrInfo and TargetInstrInfo.
    
    At this is an empty shell, but it is required to connect the codegen
    pipeline.
    
    It will be filled in later.

--- llvm/lib/Target/ASAHI/MCTargetDesc/ASAHIMCTargetDesc.cpp
+++ llvm/lib/Target/ASAHI/MCTargetDesc/ASAHIMCTargetDesc.cpp
@@ -2,6 +2,7 @@
 #include "ASAHIMCTargetDesc.h"
 #include "TargetInfo/ASAHITargetInfo.h" // getTheASAHITarget
 #include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
+#include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/TargetRegistry.h"
@@ -23,11 +24,20 @@ static MCRegisterInfo *createASAHIMCRegisterInfo(const Triple &Triple) {
   return X;
 }
 
+static MCInstrInfo *createASAHIMCInstrInfo() {
+  MCInstrInfo *X = new MCInstrInfo();
+  // TODO: Fill out the instr info.
+  return X;
+}
+
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeASAHITargetMC() {
     Target &TheTarget = getTheASAHITarget();
 
     // Register the MC register info.
     TargetRegistry::RegisterMCRegInfo(TheTarget, createASAHIMCRegisterInfo);
+
+    // Register the MC instruction info.
+    TargetRegistry::RegisterMCInstrInfo(TheTarget, createASAHIMCInstrInfo);
 
     // Register the MC subtarget info.
     TargetRegistry::RegisterMCSubtargetInfo(TheTarget, createASAHIMCSubtargetInfo);
