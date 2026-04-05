89a0bfcde83145672efeea9dabd326cc8f17a02c
Prashanth Srikanth Pujar <prashanth.pujar.usc@gmail.com>
2026-04-04 20:54:54 -0700

    [ASAHI] Plugged the MCRegisterInfo
    This is just an empty shell, but it is required to plug the codegen
    pipeline.

--- llvm/lib/Target/ASAHI/MCTargetDesc/ASAHIMCTargetDesc.cpp
+++ llvm/lib/Target/ASAHI/MCTargetDesc/ASAHIMCTargetDesc.cpp
@@ -3,6 +3,7 @@
 #include "TargetInfo/ASAHITargetInfo.h" // getTheASAHITarget
 #include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
 #include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/TargetParser/Triple.h"
 
@@ -16,8 +17,17 @@ createASAHIMCSubtargetInfo(const Triple &TT, StringRef CPU, StringRef FS) {
     return createASAHIMCSubtargetInfoImpl(TT, CPU, /*TuneCPU*/ CPU, FS);
 }
 
+static MCRegisterInfo *createASAHIMCRegisterInfo(const Triple &Triple) {
+  MCRegisterInfo *X = new MCRegisterInfo();
+  // TODO: Fill out the register info.
+  return X;
+}
+
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeASAHITargetMC() {
     Target &TheTarget = getTheASAHITarget();
+
+    // Register the MC register info.
+    TargetRegistry::RegisterMCRegInfo(TheTarget, createASAHIMCRegisterInfo);
 
     // Register the MC subtarget info.
     TargetRegistry::RegisterMCSubtargetInfo(TheTarget, createASAHIMCSubtargetInfo);
